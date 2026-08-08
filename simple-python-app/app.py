3820e4b803419ca15a87cc1560919ad2cb627bfb
connect4sudheer <100154467+connect4sudheer@users.noreply.github.com>
2026-08-08 16:11:22 +0530

    Update app.py

--- simple-python-app/app.py
+++ simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! Good evening'
+    return 'Hello, world! Good Afternoon. This is first pipline testing'
 
 
 if __name__ == '__main__':
